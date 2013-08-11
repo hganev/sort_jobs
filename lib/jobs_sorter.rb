7a297726cbf5d5e3fdc554731261cace6425d32c
Hristo Ganev <hristo.b.ganev@gmail.com>
2013-08-11 15:06:06 +0200

    Not dependency hash case covered

--- lib/jobs_sorter.rb
+++ lib/jobs_sorter.rb
@@ -2,6 +2,13 @@ module JobSorter
 	class << self
 		def sort jobs
 			return [] if jobs.nil? || jobs.empty?
+			dependency_hash = dependencies(jobs)
+			return jobs.keys if dependency_hash.empty?
+		end
+
+		def dependencies hash
+			dependency_hash = hash.map { |key, value| value[0] }
+			dependency_hash.compact
 		end
 	end
 end
--- spec/sort_jobs_spec.rb
+++ spec/sort_jobs_spec.rb
@@ -9,5 +9,10 @@ describe "Sort jobs" do
   it "returns an empty sequence" do
     JobSorter::sort(@jobs).should be_empty
   end
- 
+
+  it "returns a single job" do 
+  	single_job_hash = @jobs.merge "a" => ""
+ 	JobSorter::sort(single_job_hash).should == ["a"]
+ end
+
 end
