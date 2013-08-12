262635e2ca52aa6fcf543831310b59d3f5f83dd8
Hristo Ganev <hristo.b.ganev@gmail.com>
2013-08-12 09:51:04 +0200

    One dependency case - solution

--- lib/jobs_sorter.rb
+++ lib/jobs_sorter.rb
@@ -1,14 +1,27 @@
 module JobSorter
 	class << self
 		def sort jobs
-			return [] if jobs.nil? || jobs.empty?
+			raise "Jobs should not be nil" if jobs.nil?
+			sorted_jobs = jobs.keys
+
 			dependency_hash = dependencies(jobs)
-			return jobs.keys if dependency_hash.empty?
+			p dependency_hash
+			return sorted_jobs if dependency_hash.empty?
+			reorder(sorted_jobs, dependency_hash)
 		end
 
 		def dependencies hash
-			dependency_hash = hash.map { |key, value| value[0] }
-			dependency_hash.compact
+			dependency_hash = hash.select { |key, value| value[0] }
+		end
+
+		def reorder(jobs, dependencies)
+			reordered_jobs = jobs.dup
+			jobs.each_with_index do |job_letter, index|
+				if dependencies.keys.include?(job_letter)
+					reordered_jobs[index,index] = dependencies.assoc(job_letter).reverse			
+				end
+			end
+			reordered_jobs.uniq	
 		end
 	end
 end
